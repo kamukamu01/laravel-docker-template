4f8c00b91edf2433a7590640ac33b75761097f7b
Kamesuke01 <150576767+kamukamu01@users.noreply.github.com>
2024-03-22 11:40:24 +0900

    add: todo作成画面表示用のviewsファイルを作成し、ルートを定義。

--- src/routes/web.php
+++ src/routes/web.php
@@ -10,7 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/todo/create', function () {
+    return view('todo.create');
+})->name('todo.create');
